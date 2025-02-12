470a2097668ab5e28ee6fa9468aa3b24e49b957d
Henrique <hnrsousr@gmail.com>
2025-02-12 10:12:47 -0300

    Create historia.js

---
+++ frontend/historia.js
@@ -0,0 +1,15 @@
+document.querySelector('.enviar-hist').addEventListener('click', function() {
+    const inputHist = document.querySelector('.input-hist');
+    const inputText = inputHist.value.trim();
+
+    if (inputText !== '') {
+        const balao = document.createElement('div');
+        balao.classList.add('balao-p');
+        balao.textContent = inputText;
+
+        const historiasContadas = document.querySelector('.historias-contadas');
+        historiasContadas.appendChild(balao);
+
+        inputHist.value = '';
+    }
+});
