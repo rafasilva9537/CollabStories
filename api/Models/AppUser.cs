de0eb79951d8b15637a8cd8be5b39f7f0f31266b
Rafael <rafasilva9537@gmail.com>
2025-03-21 17:10:50 -0300

    Profile image column in User

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -12,6 +12,7 @@ public class AppUser : IdentityUser<int>
     public string Nickname { get; set; } = string.Empty;
     public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow;
     public string Description { get; set; } = string.Empty;
+    public string ProfileImage { get; set; } = string.Empty;
 
     public ICollection<Story> Stories { get; set; } = [];
     public ICollection<StoryPart> StoryParts { get; set; } = [];
