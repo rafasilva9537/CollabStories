7cf430cf7910664f537ab9ee35bc862606737c76
Rafael <rafasilva9537@gmail.com>
2025-04-03 13:09:40 -0300

    Move api projects to backend directory

--- api/utils/Sql/InsertQueries.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Story(Title, Description, CreatedDate, UpdatedDate, MaximumAuthors, TurnDurationSeconds) VALUES
-('Bird ', '', '2024-02-13', '2024-02-13', 5, 60),
-('Fire in Bubbles', 'Journey of a group in search of a jewel', '', '', 5, 60);
-
-SELECT * FROM Story;
--- api/utils/Sql/UserStories.sql
+++
@@ -1,4 +0,0 @@
-SELECT TOP (1000) au.Id as userId, au.UserName, s.Title, s.CreatedDate
-FROM Story as s
-FULL OUTER JOIN AppUser as au
-	ON s.UserId = au.Id;
