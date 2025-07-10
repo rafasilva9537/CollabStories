f0c341030ea3a7a6747fe85709d152702e37666b
rafasilva9537 <rafasilva9537@gmail.com>
2025-07-10 05:46:14 -0300

    Enhance `StoryHub` with logging, story existence checks, and improved user validation

--- backend/api.IntegrationTest/Hubs/StoryHubTests.cs
+++ backend/api.IntegrationTest/Hubs/StoryHubTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Text.Json;
 using api.Constants;
+using api.Dtos.StoryPart;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Client;
 using Xunit.Abstractions;
 
@@ -24,15 +26,74 @@ public class StoryHubTests : IClassFixture<CustomWebAppFactory>
     }
 
     [Fact]
-    public async Task JoinStorySession_WhenStoryExists_ReturnsSuccess()
+    public async Task JoinStorySession_WhenStoryExists_JoinsSuccessfully()
     {
-        HubConnection connection = _factory.CreateHubConnectionWithAuth(
-            "neva_rosenbaum29", 
-            "neva_rosenbaum29", 
-            "neva_rosenbaum2930@hotmail.com", 
+        // Arrange
+        await using HubConnection connection = _factory.CreateHubConnectionWithAuth(
+            "neva_rosenbaum29",
+            "neva_rosenbaum29",
+            "neva_rosenbaum2930@hotmail.com",
             RoleConstants.User
         );
 
+        // Act/Assert
         await connection.StartAsync();
+        await connection.InvokeAsync("JoinStorySession", "neva_rosenbaum29", 158);
+    }
+
+    [Theory]
+    [InlineData("JoinStory", 158)]
+    [InlineData("JoinStorySession", "158")]
+    [InlineData("JoinStorySession", null)]
+    public async Task JoinStorySession_WithInvalidArguments_ThrowsException(string methodName, object storyId)
+    {
+        // Arrange
+        await using HubConnection connection = _factory.CreateHubConnectionWithAuth(
+            "neva_rosenbaum29",
+            "neva_rosenbaum29",
+            "neva_rosenbaum2930@hotmail.com",
+            RoleConstants.User
+        );
+
+        // Act
+        await connection.StartAsync();
+        
+        // Assert
+        Task InvokeHubMethod() => connection.InvokeAsync(methodName, storyId);
+        await Assert.ThrowsAsync<HubException>(InvokeHubMethod);
+    }
+
+    [Fact]
+    public async Task SendStoryPart_WhenConnectedToStory_OtherUsersReceiveStoryPart()
+    {
+        // Arrange
+        await using HubConnection connection1 = _factory.CreateHubConnectionWithAuth(
+            "neva_rosenbaum29",
+            "neva_rosenbaum29",
+            "neva_rosenbaum2930@hotmail.com",
+            RoleConstants.User
+        );
+        await connection1.StartAsync();
+        await connection1.InvokeAsync("JoinStorySession", 158);
+        
+        await using HubConnection connection2 = _factory.CreateHubConnectionWithAuth(
+            "jovani.lueilwitz",
+            "jovani.lueilwitz",
+            "jovani.lueilwitz77@gmail.com",
+            RoleConstants.User
+        );
+        await connection2.StartAsync();
+        await connection2.InvokeAsync("JoinStorySession", 158);
+        
+        // Act
+        await connection1.InvokeAsync("SendStoryPart", 158, "Test story part");
+        
+        // Assert
+        connection2.On<int, StoryPartDto>("ReceiveStoryPart", (storyId, receivedStoryPart) =>
+        {
+            Assert.Equal(158, storyId);
+            Assert.Equal("Test story part", receivedStoryPart.Text);
+            Assert.Equal("neva_rosenbaum29", receivedStoryPart.UserName);
+        });
     }
 }
