d6480bc25875632cc41e2ee7482dedd71165dc10
rafasilva9537 <rafasilva9537@gmail.com>
2025-07-13 20:14:24 -0300

    Add test data classes `AuthorsInStoryTestData` and `TestUserModel` for integration tests

---
+++ backend/api.IntegrationTest/Data/AuthorsInStoryTestData.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace api.IntegrationTests.Data;
+
+public class AuthorsInStoryTestData : IEnumerable<object[]>
+{
+    public const int StoryId = 158;
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[]
+        {
+            new TestUserModel
+            {
+                UserName = "neva_rosenbaum29",
+                NameIdentifier = "neva_rosenbaum29",
+                Email = "neva_rosenbaum2930@hotmail.com"
+            },
+            new TestUserModel
+            {
+                UserName = "jovani.lueilwitz",
+                NameIdentifier = "jovani.lueilwitz",
+                Email = "jovani.lueilwitz77@gmail.com"
+            },
+            new TestUserModel()
+            {
+                UserName = "fabian.fisher",
+                NameIdentifier = "fabian.fisher",
+                Email = "fabian.fisher.herman6@yahoo.com"
+            }
+        };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ backend/api.IntegrationTest/Data/TestUserModel.cs
@@ -0,0 +1,8 @@
+﻿namespace api.IntegrationTests.Data;
+
+public class TestUserModel
+{
+    public required string UserName { get; set; }
+    public required string NameIdentifier { get; set; }
+    public required string Email { get; set; }
+}
