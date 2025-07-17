d042ccbcd75c925681155489c2cca896d266fe82
rafasilva9537 <rafasilva9537@gmail.com>
2025-07-17 12:53:42 -0300

    Add `WebAppFactoryExtensions` for creating authenticated HTTP clients and SignalR hub connections

---
+++ backend/api.IntegrationTest/WebAppFactories/WebAppFactoryExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Headers;
+using api.Constants;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+
+namespace api.IntegrationTests.WebAppFactories;
+
+public static class WebAppFactoryExtensions
+{
+    public static HttpClient CreateClientWithAuth(
+        this WebApplicationFactory<Program> factory, 
+        string userName, 
+        string nameIdentifier, 
+        string email, 
+        params string[] roles)
+    {
+        HttpClient client = factory.CreateClient();
+        
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(TestAuthHandler.AuthenticationScheme);
+        client.DefaultRequestHeaders.Add(TestAuthHandler.NameHeader, userName);
+        client.DefaultRequestHeaders.Add(TestAuthHandler.NameIdentifierHeader, nameIdentifier);
+        client.DefaultRequestHeaders.Add(TestAuthHandler.EmailHeader, email);
+        foreach (string role in roles)
+        {
+            client.DefaultRequestHeaders.Add(TestAuthHandler.RoleHeader, role);
+        }
+
+        return client;
+    }
+    
+    public static HubConnection CreateHubConnectionWithAuth(
+        this WebApplicationFactory<Program> factory,
+        string userName, 
+        string nameIdentifier, 
+        string email,
+        params string[] roles
+    )
+    {
+        TestServer server = factory.Server;
+        HubConnection connection = new HubConnectionBuilder()
+            .WithUrl(server.BaseAddress + UrlConstants.StoryHub, options =>
+            {
+                options.HttpMessageHandlerFactory = _ => server.CreateHandler();
+                options.Headers.Add("Authorization", TestAuthHandler.AuthenticationScheme);;
+                options.Headers.Add(TestAuthHandler.NameHeader, userName);
+                options.Headers.Add(TestAuthHandler.NameIdentifierHeader, nameIdentifier);
+                options.Headers.Add(TestAuthHandler.EmailHeader, email);
+                foreach (string role in roles)
+                {
+                    options.Headers.Add(TestAuthHandler.RoleHeader, role);
+                }
+            })
+            .Build();
+        
+        return connection;
+    }
+}
