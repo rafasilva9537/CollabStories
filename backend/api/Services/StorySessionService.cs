2722f71211642954f13fb117ffe2370b4e1a2429
rafasilva9537 <rafasilva9537@gmail.com>
2025-07-17 12:55:45 -0300

    Refactor `StorySessionService` to use `SessionInfo` for improved session management and add support for timers and turn durations

--- backend/api/Services/StorySessionService.cs
+++ backend/api/Services/StorySessionService.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections.Concurrent;
-using System.Collections.Immutable;
+using api.Dtos.Story;
+using api.Exceptions;
+using api.Interfaces;
 
 namespace api.Services;
 
 public interface IStorySessionService : IDisposable
 {
     int SessionsCount { get; }
-    void AddSession(string storyId);
+    Task AddSessionAsync(string storyId, IStoryService storyService);
     void RemoveSession(string storyId);
     void RemoveAllSessions();
     bool SessionIsActive(string storyId);
     bool SessionIsEmpty(string storyId);
+    public int GetSessionTurnDurationSeconds(string storyId);
+    public double GetSessionTimerSeconds(string storyId);
+    public void DecrementSessionTimer(string storyId, double seconds = 1);
     void AddConnectionToSession(string storyId, string connectionId);
     void RemoveConnectionFromSession(string storyId, string connectionId);
     
     IReadOnlySet<string> GetSessionConnections(string storyId);
+}
+
+public class SessionInfo
+{
+    public readonly HashSet<string> Connections = [];
+    public double TimerSeconds { get; set; }
+    public int TurnDurationSeconds { get; init; }
+    
+    public SessionInfo(double timerSeconds, int turnDurationSeconds)
+    {
+        TimerSeconds = timerSeconds;
+        TurnDurationSeconds = turnDurationSeconds;
+    }
 }
 
 /// <summary>
@@ -27,19 +45,38 @@ public class StorySessionService : IStorySessionService
     /// A thread-safe dictionary that manages active session groups and their associated users' connection ids.
     /// Story id need to be string to be compatible with signalr group names, which are string
     /// </summary>
-    private readonly ConcurrentDictionary<string, HashSet<string>> _sessions = new();
-    private readonly ILogger<StorySessionService> _logger;
+    private readonly ConcurrentDictionary<string, SessionInfo> _sessions = new();
+    private readonly ILogger<IStorySessionService> _logger;
+    private readonly IDateTimeProvider _dateTimeProvider;
+
     public int SessionsCount { get => _sessions.Count; }
     
-    public StorySessionService(ILogger<StorySessionService> logger)
+    public StorySessionService(ILogger<IStorySessionService> logger, IDateTimeProvider dateTimeProvider)
     {
         _logger = logger;
+        _dateTimeProvider = dateTimeProvider;
     }
 
-    public void AddSession(string storyId)
+    public async Task AddSessionAsync(string storyId, IStoryService storyService)
     {
-        _sessions.TryAdd(storyId, new HashSet<string>());
-        _logger.LogInformation("Added session for group {StoryId}", storyId);
+        StoryInfoForSessionDto? story = await storyService.GetStoryInfoForSessionAsync(int.Parse(storyId));
+        if (story is null)
+        {
+            throw new NoStoryException($"Story with ID {storyId} not found.");
+        }
+
+        TimeSpan timeSinceUpdate = _dateTimeProvider.UtcNow - story.AuthorsMembershipChangeDate;
+        double remainingTimerSeconds = timeSinceUpdate.TotalSeconds;
+        int turnDurationSeconds = story.TurnDurationSeconds;
+        
+        SessionInfo sessionInfo = new SessionInfo(remainingTimerSeconds, turnDurationSeconds);
+        _sessions.TryAdd(storyId, sessionInfo);
+        
+        _logger.LogInformation(
+            "Added session for group {StoryId}, with timer starting at {TimerSeconds} seconds and turn duration of {TurnDurationSeconds} seconds.",
+            storyId,
+            remainingTimerSeconds,
+            turnDurationSeconds);
     }
     
     public void RemoveSession(string storyId)
@@ -55,7 +92,7 @@ public class StorySessionService : IStorySessionService
 
     public bool SessionIsEmpty(string storyId)
     {
-        return _sessions[storyId].Count <= 0;
+        return _sessions[storyId].Connections.Count <= 0;
     }
 
     public void RemoveAllSessions()
@@ -63,16 +100,31 @@ public class StorySessionService : IStorySessionService
         _sessions.Clear();
         _logger.LogInformation("Removed all sessions");   
     }
+
+    public int GetSessionTurnDurationSeconds(string storyId)
+    {
+        return _sessions[storyId].TurnDurationSeconds;
+    }
+
+    public double GetSessionTimerSeconds(string storyId)
+    {
+        return _sessions[storyId].TimerSeconds;
+    }
+
+    public void DecrementSessionTimer(string storyId, double seconds = 1)
+    {
+        _sessions[storyId].TimerSeconds -= seconds;
+    }
     
     public void AddConnectionToSession(string storyId, string connectionId)
     {
-        _sessions[storyId].Add(connectionId);
+        _sessions[storyId].Connections.Add(connectionId);
         _logger.LogInformation("Added connection {ConnectionId} to session for group {StoryId}", connectionId, storyId);
     }
     
     public void RemoveConnectionFromSession(string storyId, string connectionId)
     {
-        _sessions[storyId].Remove(connectionId);
+        _sessions[storyId].Connections.Remove(connectionId);
         _logger.LogInformation("Removed connection {ConnectionId} from session for group {StoryId}", connectionId, storyId);
     }
     
@@ -83,10 +135,10 @@ public class StorySessionService : IStorySessionService
     /// <returns>A read-only set of connection IDs for the specified session.</returns>
     public IReadOnlySet<string> GetSessionConnections(string storyId)
     {
-        IReadOnlySet<string> connections = _sessions[storyId];
+        IReadOnlySet<string> connections = _sessions[storyId].Connections;
         
         _logger.LogDebug("Returning connections for group {StoryId}", storyId);
-        return connections;   
+        return connections;
     }
 
     public void Dispose()
