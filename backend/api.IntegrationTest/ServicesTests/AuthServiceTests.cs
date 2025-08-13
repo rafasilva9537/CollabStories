7901f06f3a8e188944dd27de974925df47a106c9
rafasilva9537 <rafasilva9537@gmail.com>
2025-08-13 16:22:45 -0300

    Add `AuthServiceTests` class with integration test setup using `AuthHandlerWebAppFactory`.

---
+++ backend/api.IntegrationTest/ServicesTests/AuthServiceTests.cs
@@ -0,0 +1,13 @@
+﻿using api.IntegrationTests.WebAppFactories;
+
+namespace api.IntegrationTests.ServicesTests;
+
+public class AuthServiceTests : IClassFixture<AuthHandlerWebAppFactory>
+{
+    private readonly AuthHandlerWebAppFactory _factory;
+    
+    public AuthServiceTests(AuthHandlerWebAppFactory factory)
+    {
+        _factory = factory;
+    }
+}
