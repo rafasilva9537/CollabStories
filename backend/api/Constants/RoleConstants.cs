353f58480f9a488452fd131671480104fe84c12c
rafa <100155926+rafasilva9537@users.noreply.github.com>
2025-11-21 02:41:45 -0300

    Add TODO for User role removal in RoleConstants
    Added TODO comment to consider removing User role.

--- backend/api/Constants/RoleConstants.cs
+++ backend/api/Constants/RoleConstants.cs
@@ -2,6 +2,7 @@ namespace api.Constants;
 
 public class RoleConstants {
     // User role exists to create a way to block accounts, if you need to revoke the entire auth access of an account from a normal user, just remove the role
+    // #TODO: remove User role and use other way to block access, like IsActive property or User Lockout
     public const string Admin = "Admin";
     public const string User = "User";
 }
